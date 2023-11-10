c04e05049af9b83baf3985d12e2e81b3f164ace1
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-11-10 06:14:59 +0530

    Create linearprobing.c

---
+++ linearprobing.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+// Hash function
+int hash(int key) {
+    return key % SIZE;
+}
+
+// Insert a key into the hash table using linear probing
+void insert(int hashTable[], int key) {
+    int index = hash(key);
+
+    // Linear probing until an empty slot is found
+    while (hashTable[index] != -1) {
+        index = (index + 1) % SIZE;
+    }
+
+    // Insert the key into the hash table
+    hashTable[index] = key;
+}
+
+// Display the hash table
+void display(int hashTable[]) {
+    printf("Hash Table:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d: %d\n", i, hashTable[i]);
+    }
+}
+
+int main() {
+    int hashTable[SIZE];
+
+    // Initialize the hash table with -1 (indicating empty slots)
+    for (int i = 0; i < SIZE; i++) {
+        hashTable[i] = -1;
+    }
+
+    // Insert keys into the hash table
+    insert(hashTable, 5);
+    insert(hashTable, 25);
+    insert(hashTable, 15);
+    insert(hashTable, 35);
+
+    // Display the final hash table
+    display(hashTable);
+
+    return 0;
+}
