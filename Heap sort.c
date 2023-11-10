319f2ecb5bb522231cb81f71f31998031ca1d17a
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-11-10 07:07:05 +0530

    Create Heap sort.c

---
+++ Heap sort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+// Function to swap two elements in the array
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to heapify a subtree rooted at given index
+void heapify(int arr[], int size, int index) {
+    int largest = index;
+    int leftChild = 2 * index + 1;
+    int rightChild = 2 * index + 2;
+
+    if (leftChild < size && arr[leftChild] > arr[largest]) {
+        largest = leftChild;
+    }
+
+    if (rightChild < size && arr[rightChild] > arr[largest]) {
+        largest = rightChild;
+    }
+
+    if (largest != index) {
+        swap(&arr[index], &arr[largest]);
+        heapify(arr, size, largest);
+    }
+}
+
+// Function to perform Heap Sort
+void heapSort(int arr[], int size) {
+    // Build max heap
+    for (int i = size / 2 - 1; i >= 0; i--) {
+        heapify(arr, size, i);
+    }
+
+    // Extract elements from the heap one by one
+    for (int i = size - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+// Function to display the sorted array
+void displayArray(int arr[], int size) {
+    printf("Sorted Array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    heapSort(arr, size);
+
+    displayArray(arr, size);
+
+    return 0;
+}
